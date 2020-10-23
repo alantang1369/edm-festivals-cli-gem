4cb1e1f6cbbaeb0592dae5443550ee94fe496ffc
alantang1369 <giabao_tang@yahoo.com>
2020-10-23 06:44:17 -0700

    commit untracked

---
+++ lib/edm_festivals/cli.rb
@@ -0,0 +1,7 @@
+class EdmFestivals::CLI 
+    def call 
+        puts "Welcome to the electric sky!"
+        puts "Here is the top 100 festivals around the world that you never wanna miss:"
+    end
+end
+
